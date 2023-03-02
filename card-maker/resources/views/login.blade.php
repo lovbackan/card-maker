3867c678d847a5726ed87f8b2807b1c7cc8d4a9b
lovbackan <simon@lovbacka.se>
2023-03-02 16:18:08 +0100

    added a new folder for the specific login js and css

--- card-maker/resources/views/login.blade.php
+++ card-maker/resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/login/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -67,10 +67,9 @@
 </section>
 
 
-<script src="/js/script.js"></script>
+<script src="/js/login/script.js"></script>
 </body>
 </html>
 
 
 @include('errors')
-{{-- @include('errors') --}}
