57c9b594ab3a435dbb62cb5665a658864a577a3d
lovbackan <simon@lovbacka.se>
2023-03-07 15:08:17 +0100

    made a partials for form!

--- card-maker/resources/views/dashboard.blade.php
+++ card-maker/resources/views/dashboard.blade.php
@@ -8,7 +8,8 @@
     <title>Card-maker</title>
 </head>
 <body>
-<section>
+
+<section class="sidebarSection">
     <div class="sidebar">
 <a href="/logout">Log out</a>
 <p>Hello, {{ $user->name;}}!</p>
@@ -20,32 +21,27 @@
 
 </section>
 
-<section>
+<section class="createCardSection">
 
 <div class="createCardContainer">
     <h1>Create card </h1>
 <form method="POST"class="createCard" action="/createCard">
-<label for="category">Card category </label>
-<select name="category" id="category">
-    @foreach ($categories as $category)
-
-    <option value="{{$category->category_name}}">{{$category->category_name}}</option>
-
-    @endforeach
-</select>
-<label for="title">Title</label>
-<input name="title" id="title" type="text" />
-<label for="body">Body</label>
-<textarea name="body" id="textBody" rows="20" cols="30">
-</textarea>
+    @include('partials.form-fields')
 <button class="cardSubmit" type="submit">Create card</button>
-<input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
 </form>
 </div>
 
 </section>
 
-<section>
+<section class="editCardSection">
+    <form method="POST"class="editCard" action="/editCard">
+
+
+<input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+    </form>
+</section>
+
+<section class="cardDisplayerSection">
     <div class="cardDisplayer">
         @foreach ($cards as $card)
         <div class="cardContainer">
