5c81f230759c1a7374f89415db3733adc209c553
lovbackan <simon@lovbacka.se>
2023-03-07 11:11:43 +0100

    added a create card button

--- card-maker/public/js/dashboard/script.js
+++ card-maker/public/js/dashboard/script.js
@@ -1,2 +1,9 @@
 const createCardButton = document.querySelector(".createCardButton");
-console.log(createCardButton);
+const createCardContainer = document.querySelector(".createCardContainer");
+console.log(createCardButton, createCardContainer);
+
+createCardButton.addEventListener("click", function () {
+    console.log("click");
+
+    document.querySelector(".createCardContainer").classList.toggle("active");
+});
