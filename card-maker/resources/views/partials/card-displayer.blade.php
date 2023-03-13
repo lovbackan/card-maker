13afe848233181e3aa72368e29a5753c0370617f
lovbackan <simon@lovbacka.se>
2023-03-13 15:05:59 +0100

    if you want icons for the categories you can now replace the specific icon in the icon folder

--- card-maker/resources/views/partials/card-displayer.blade.php
+++ card-maker/resources/views/partials/card-displayer.blade.php
@@ -4,7 +4,12 @@
             <h2>{{$card->title}} </h2>
             </div>
             <div class="category">
+
                 <h3>{{$card->category->category_name}}</h3>
+
+            </div>
+            <div class="categoryIcon">
+                <img src="/icons/{{$card->category->category_name}}.svg">
             </div>
             <div class="textBody">
             <p>{{$card->body}}</p>
