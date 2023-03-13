90bf7336f7d144baa0d2b5a696afc18f17ddc94f
Styrbjörn Nordberg <styris.n@gmail.com>
2023-03-13 17:48:13 +0100

    styled create and edit card sections

--- card-maker/resources/views/partials/create-card-container.blade.php
+++ card-maker/resources/views/partials/create-card-container.blade.php
@@ -1,19 +1,23 @@
-<h1>Create card </h1>
-            <form method="POST" class="createCard" action="/createCard">
-<label for="category">Card category </label>
-<select name="category" id="category">
-    @foreach ($categories as $category)
+<h1>Create card</h1>
+<form method="POST" class="createCard" action="/createCard">
+    <select class="cardCategory" name="category" id="category">
+        <option style="display:none;">category</option>
+        @foreach ($categories as $category)
 
-    <option value="{{$category->category_name}}">{{$category->category_name}}</option>
+        <option value="{{$category->category_name}}">{{$category->category_name}}</option>
 
-    @endforeach
-</select>
-<label for="title">Title</label>
-<input name="title" id="title" type="text" />
-<label for="body">Body</label>
-<textarea name="body" id="textBody" rows="20" cols="30">
-</textarea>
-<input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+        @endforeach
+    </select>
+    <div class="titleWraper">
+        <label for="title">Title</label>
+        <input name="title" id="title" type="text" />
+    </div>
 
-<button class="cardSubmit" type="submit">Create card</button>
+    <div class="bodyWraper">
+        <label for="body">Body</label>
+        <textarea name="body" id="textBody" rows="20" cols="30"></textarea>
+    </div>
+    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+
+    <button class="cardSubmit" type="submit">Create card</button>
 </form>
--- card-maker/resources/views/partials/edit-card-container.blade.php
+++ card-maker/resources/views/partials/edit-card-container.blade.php
@@ -1,23 +1,30 @@
 <h1>Edit card </h1>
-            <form method="POST" class="editCard" action="/editCard">
-<label for="cardSelector">Card selector </label>
-                <select name="cardSelector" id="cardSelector">
-                    <option selected="true" disabled="disabled">Select Card</option>
-                    @foreach ($cards as $card)
-                    <option value="{{$card->title}}">{{$card->title}}</option>
-                    @endforeach
-                </select>
-                <label for="category">Card category </label>
-                <select name="category" id="editCategory">
-                    @foreach ($categories as $category)
-                    <option value="{{$category->category_name}}">{{$category->category_name}}</option>
-                    @endforeach
-                </select>
-                <label for="title">Title</label>
-                <input name="title" id="editTitle" type="text" />
-                <label for="body">Body</label>
-                <textarea name="body" id="editTextBody" rows="20" cols="30"></textarea>
-                <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
-        <button class="editCardSubmit" type="submit">Edit card</button>
-    </form>
+<form method="POST" class="editCard" action="/editCard">
+
+    <select class="cardCategory" name="cardSelector" id="cardSelector">
+        <option style="display: none;">Select Card</option>
+        @foreach ($cards as $card)
+        <option value="{{$card->title}}">{{$card->title}}</option>
+        @endforeach
+    </select>
+
+    <select class="cardCategory" name="category" id="editCategory">
+        @foreach ($categories as $category)
+        <option value="{{$category->category_name}}">{{$category->category_name}}</option>
+        @endforeach
+    </select>
+
+    <div class="titleWraper">
+        <label for="title">Title</label>
+        <input name="title" id="editTitle" type="text" />
+    </div>
+
+    <div class="bodyWraper">
+        <label for="body">Body</label>
+        <textarea name="body" id="editTextBody" rows="20" cols="30"></textarea>
+    </div>
+    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+
+    <button class="editCardSubmit" type="submit">Edit card</button>
+</form>
 </div>
