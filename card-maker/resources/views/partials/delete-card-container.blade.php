1995485dbdb2b08ef78e00ba577a6ea90aaf936f
Styrbjörn Nordberg <styris.n@gmail.com>
2023-03-14 12:51:59 +0100

    Styled delete card

--- card-maker/resources/views/partials/delete-card-container.blade.php
+++ card-maker/resources/views/partials/delete-card-container.blade.php
@@ -1,12 +1,12 @@
 <h1> Delete Card </h1>
-<form method="POST"class="deleteCard" action="/deleteCard">
-<label for="cardSelector">Card selector </label>
-<select name="cardSelector" id="cardSelector">
-    @foreach ($cards as $card)
-    <option value="{{$card->title}}">{{$card->title}}</option>
-    @endforeach
-</select>
-<input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
-<button class="deleteCardSubmit" type="submit">Delete card</button>
+<form method="POST" class="deleteCard" action="/deleteCard">
+    <select name="cardSelector" id="cardSelector">
+        <option style="display: none;">Select card</option>
+        @foreach ($cards as $card)
+        <option value="{{$card->title}}">{{$card->title}}</option>
+        @endforeach
+    </select>
+    <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
+    <button class="deleteCardSubmit" type="submit">Delete card</button>
 </form>
 </div>
